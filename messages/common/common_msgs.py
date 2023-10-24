f8934703017d2a8ea43d3d2dd9514555d835ec26
NotThatDroids <daniil.tarkov@advncd@gmail.com>
2023-10-24 23:08:57 +0600

    help_message_added

--- messages/common/common_msgs.py
+++ messages/common/common_msgs.py
@@ -13,6 +13,13 @@ def start_message(chat_id):
 
 def help_message(chat_id):
     method = "sendMessage"
-    text = "help text"
-    data = {"chat_id": chat_id, "text": text}
+    text = "*Краткая помощь:*\n" \
+           "/start - приветственное сообщение.\n" \
+           "/setprofile - создать/изменить профиль.\n" \
+           "/cancel - отменить создание профиля.\n\n" \
+           "*Только после создания профиля:*\n" \
+           "/viewprofile - посмотреть свой профиль.\n" \
+           "/find - найти пирата.\n" \
+           "/shuffle - начать поиск заново."
+    data = {"chat_id": chat_id, "text": text, "parse_mode": "Markdown"}
     send_message(method, data=data)
